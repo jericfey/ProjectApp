dcb194af170338844b789e390d749f71366fc7f6
Grace Kuo <gkuo.github@gmail.com>
2021-01-14 19:26:53 -0700

    changed server.js

--- server.js
+++ server.js
@@ -20,11 +20,6 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-const routes = require("./config/connection");
-
-app.use(routes);
-
 // Const for API route:
 const apiRoutes = require("./routes/api-routes");
 
