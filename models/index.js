9f7904acfd2820ace87a1668e8d3345364d6edce
Jeri Fey <jericfey@gmail.com>
2021-01-14 20:03:18 -0700

    Dugh

---
+++ models/index.js
@@ -0,0 +1,4 @@
+const Sequelize = require("sequelize");
+// sequelize (lowercase) references my connection to the DB.
+const sequelize = require("../config/connection.js");
+
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Requiring our models for syncing - for sequelize
-const db = require('./models');
+const db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -21,7 +21,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require("./controllers/catsController.js");
+const routes = require("./routes/api-routes.js");
 
 app.use(routes);
 
